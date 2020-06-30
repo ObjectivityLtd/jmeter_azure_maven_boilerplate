7369182a8d408143463032e70146d18342dde33e
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-06-30 09:25:11 +0200

    update BATS installation script

--- src/test/azure/modules/bats/install.sh
+++ src/test/azure/modules/bats/install.sh
@@ -3,9 +3,10 @@
 #we can install that on the fly for CI systems or we can add this in docker image
 function installBATS(){
   clear && rm -rf $HOME/test/test_helper/batscore  && rm -rf $HOME/test/test_helper/batsassert
-  git clone https://github.com/ztombol/bats-core $HOME/test/test_helper/batscore
+  git clone https://github.com/bats-core/bats-core.git $HOME/test/test_helper/batscore
   git clone https://github.com/ztombol/bats-assert $HOME/test/test_helper/batsassert
   ls $HOME/test/test_helper/batscore
-  source $HOME/test/test_helper/batscore/script/install-bats.sh ~
+  cd $HOME/test/test_helper/batscore
+  ./install.sh $HOME
 }
 installBATS
