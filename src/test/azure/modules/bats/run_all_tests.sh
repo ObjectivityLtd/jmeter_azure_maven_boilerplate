5bbe4a2fc80411ea88ae9c79790071385a6a86a2
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-07-01 11:54:56 +0200

    adding bats to PATH

--- src/test/azure/modules/bats/run_all_tests.sh
+++ src/test/azure/modules/bats/run_all_tests.sh
@@ -3,17 +3,20 @@
 function runModuleTests(){
  local here=$(pwd)
  local module=$1
+ local test_folder=$2
  printf "\n# Running tests for module $module\n"
- cd ../$module && bats *.bats
+ cd ../$module && bats -o "$test_folder" -F junit *.bats
  cd "$here"
 }
 
 function runAll(){
-  runModuleTests artifacts
-  runModuleTests chatops
-  runModuleTests reports
-  runModuleTests thresholds
-  runModuleTests templates
+  result_folder=$(pwd)/tmp
+  mkdir -p "$result_folder"
+  runModuleTests artifacts "$result_folder" && \
+  runModuleTests chatops "$result_folder" && \
+  runModuleTests reports "$result_folder" && \
+  runModuleTests thresholds "$result_folder" && \
+  runModuleTests templates "$result_folder"
 }
 
 runAll
